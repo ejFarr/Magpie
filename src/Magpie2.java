0593b8730bf9ae5cfba18de363032478e763d07a
Evan Farr <roboproductions200@gmail.com>
2019-11-15 12:49:38 -0700

    Magpie 2 exercises

--- src/Magpie2.java
+++ src/Magpie2.java
@@ -38,6 +38,17 @@ public class Magpie2 {
 				|| statement.indexOf("sister") >= 0
 				|| statement.indexOf("brother") >= 0) {
 			response = "Tell me more about your family.";
+		} else if (statement.indexOf("cat") >= 0 
+				|| statement.indexOf("dog") >=0) {
+			response = "Tell me more about your pets";
+		} else if (statement.indexOf("padjen") >=0) {
+			response = "He sounds like a grat person";
+		} else if (statement.trim().length() < 1) {
+			response = "Say something, please.";
+		} else if(statement.indexOf("fortnite") >= 0) {
+			response = "OK Boomer.";
+		} else if (statement.indexOf("epic") >= 0) {
+			response = "Are you a GAMER?";
 		} else {
 			response = getRandomResponse();
 		}
@@ -50,7 +61,7 @@ public class Magpie2 {
 	 * @return a non-committal string
 	 */
 	private String getRandomResponse() {
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
 		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
 		String response = "";
@@ -63,6 +74,10 @@ public class Magpie2 {
 			response = "Do you really think so?";
 		} else if (whichResponse == 3) {
 			response = "You don't say.";
+		} else if (whichResponse == 4) {
+			response = "Life never gave you lemons, they are a hybrid of citron and oranges, we made them ourselves.";
+		} else if (whichResponse == 5) {
+			response = "Are you sure?";
 		}
 
 		return response;
